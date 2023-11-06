dd545f15a9ac8b6cc7a04d6c3679c83d49ce8920
infantGrandpa <babraham341@gmail.com>
2023-11-06 11:43:22 -0500

    Improve mobile experience for hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: '3rem',
+        DEFAULT: '1.5rem',
         sm: '2rem',
         lg: '4rem',
         xl: '5rem',
