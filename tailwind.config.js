fd581c134ff1f0265a0e3b87def0c775420f4850
infantGrandpa <babraham341@gmail.com>
2023-11-07 10:17:49 -0500

    Test Containers Again

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,7 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1.5rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
+      padding: '2rem',
       screens: {
         '2xl': '1350px',
       }
