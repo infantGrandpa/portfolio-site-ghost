d890f6d38635ec5e35be83804c133d60d39b8b68
infantGrandpa <babraham341@gmail.com>
2023-11-07 10:51:04 -0500

    Add Screen Sizes to provide Breakpoint Definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/js/main.js","./**/*.{html,hbs}"],
-  darkMode: 'class',
-  theme: {
-    fontFamily: {
-      'body': ['Montserrat', 'sans-serif']
+    content: ["./assets/js/main.js", "./**/*.{html,hbs}"],
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            body: ["Montserrat", "sans-serif"],
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1.5rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                "2xl": "1536px",
+            },
+        },
     },
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1350px',
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
+    ],
+};
